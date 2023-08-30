441885a5e540224dfb068e9184a6ed2ada135144
raja <memonabdulrehman250@gmail.com>
2023-08-30 22:15:36 +0500

    third

--- src/components/MyIntro.jsx
+++ src/components/MyIntro.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { styled } from 'styled-components';
-import profile from "../images/profile.jpg";
+import profile from "../images/profile.jpeg";
 
 
 const MyIntro = () => {
