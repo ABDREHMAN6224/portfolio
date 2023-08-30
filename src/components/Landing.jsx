e9cfdf7019c802faec07963c5c701bd5ce80eb07
raja <memonabdulrehman250@gmail.com>
2023-08-30 22:24:49 +0500

    fourth

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { styled } from 'styled-components'
-import profile from "../images/profile.jpg"
 import MyIntro from './MyIntro'
 import AboutMe from './AboutMe'
 import Services from './Services'
