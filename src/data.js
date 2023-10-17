695d488c68c40d0765db633c0a6381e4a4fd8bda
raja <memonabdulrehman250@gmail.com>
2023-10-18 00:22:52 +0500

    proj

--- src/data.js
+++ src/data.js
@@ -1,9 +1,16 @@
 import mart from "./images/mart.png";
+import unsplash from "./images/unsplash.png";
 export const projects = [
     {
         img: mart,
         name: "Nust Laptops",
         github: "https://github.com/ABDREHMAN6224/AR-Mart",
         website: "https://ar-emart-arm.netlify.app/"
+    },
+    {
+        img: unsplash,
+        name: "Unsplash Images",
+        github: "https://github.com/ABDREHMAN6224/vite-images",
+        website: "https://unsplash-images-arm.netlify.app/"
     }
 ]
