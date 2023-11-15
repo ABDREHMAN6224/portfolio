f1fae837bf5539c70b4a7af9cf220e860719b6e8
raja <memonabdulrehman250@gmail.com>
2023-11-15 21:17:16 +0500

    update

--- src/data.js
+++ src/data.js
@@ -1,12 +1,19 @@
 import mart from "./images/mart.png";
 import unsplash from "./images/unsplash.png";
 import gitdrill from "./images/gitdrill.png";
+import ar_chat from "./images/ar_chat.png";
 export const projects = [
     {
         img: mart,
         name: "Nust Laptops",
         github: "https://github.com/ABDREHMAN6224/AR-Mart",
         website: "https://ar-emart-arm.netlify.app/"
+    },
+    {
+        img: ar_chat,
+        name: "Ar Chats (uses Sockets)",
+        github: "https://github.com/ABDREHMAN6224/chat-frontend",
+        website: "https://archats-arm.netlify.app/"
     },
     {
         img: unsplash,
